f56757f47f7b40e01ed4c4412433d2c6d5c82813
alesnow97 <aleruss97@gmail.com>
2020-07-30 23:20:39 +0200

    windeffectfunction update

--- Model/eqns_vx_tyres.m
+++
@@ -1,9 +0,0 @@
-syms xdot a b psidot psi ydot vxR vxF delta
-
-
-vxR = (xdot + b*psidot*sin(psi))*cos(psi) + (ydot - b*psidot*cos(psi))*sin(psi);
-
-vxF = (xdot - b*psidot*sin(psi))*cos(psi+delta) + (ydot + a*psidot*cos(psi))*sin(psi+delta);
-
-simplify(vxR)
-simplify(vxF)
