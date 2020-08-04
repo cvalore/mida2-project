fdb0cd2d296f72b69a06756216446a80d691738a
Carmelo <cvalore7@gmail.com>
2020-08-04 10:47:11 +0200

    updated Model

--- Model/Init.m
+++ Model/Init.m
@@ -1,7 +1,7 @@
 %% Tyre setup
 a0 = 1.4;       % [-]       --------Shape factor
 a1 = 0;         % [1/kN]    --------Load influence on lateral friction coefficient (*1000)
-a2 = 2000;       % [-]       --------Lateral friction coefficient (*1000)
+a2 = 2000;      % [-]       --------Lateral friction coefficient (*1000)
 a3 = 1100;      % [N/deg]   --------Change of stiffness with slip
 a4 = 10;        % [kN]      --------Change of progressivity of stiffness / load
 a5 = 0;         % [\%/deg/100]  --------Camber influence on stiffness
@@ -18,65 +18,58 @@ a15 = 0;        % [1/deg]   --------Camber influence on lateral friction coeffic
 a16 = 0;        % [-]       --------Curvature change with camber
 a17 = 0;        % [-]       -------- 	Curvature shift
 
-camber = 0;     % Camber angle
+camber = 0;     % [rad] Camber angle
 
-b1 = 0;
-b2 = 2200;
+b1 = 0;         % [1/kN]    --------Load influence on longitudinal friction coefficient (*1000)
+b2 = 2200;      % [-]       --------Longitudinal friction coefficient (*1000)
 b11 = 0;
 b12 = 0;
 
-w1 = 10^(-4.7); %wear param1
-w2 = 1; %wear param2
-
-%% Track parameters
-%global longStraight;
-%longStraight = 1006;
-%global shortStraight;
-%shortStraight = 201;
-%global turn;
-%turn = 402;
-%global curveCoord;
-%curveCoord = turn * 2 / pi;
-
-%[roadS1, roadS2, roadS3, roadS4] = plotTrack(0, 'g-', 2, 1);
-[roadS1, roadS2, roadS3, roadS4, curveRays, curveCenters, curveZ, zOffset, ...
-    xMax, xMin, yMax, yMin, bankingAngle] = track3d();
-
-%% Simulation parameters
-%Ts = 0.1;
-m_fuel = 60;
-m_vehicle = 590;
-m_passenger = 70;
-mT = m_vehicle + m_fuel + m_passenger;
-g = 9.81;
-a = 1.2;
-b = 1.6;
-IT = 1400;
-l_f = 0.45; %load distrib over front wheel
-l_r = 0.55; %load distrib over rear wheel
-
-Cx = 0.8;
-Cz = 1.5; %values to adjust
-rho_0 = 1.225; %rho non scia
-rho_1 = 0.8;%rho scia
-Area = 2;   %assuming 2m^2 of surface for the vehicle
-distanceThreshold = 10;
-overlapThreshold = 1;
-alpha = 0.7;
-
-max_vel = 50;
-init_vel = 0.01;  
-init_y_pos = -5.625;
-bankingThresholdLong = 110;
-bankingThresholdShort = 40;
-
-C_fuel = 0.0000003;
+w1 = 10^(-4.7); % [-]       --------wear param1
+w2 = 1;         % [-]       --------wear param2
 
 %tyre wears param
-%K_wear = 10^(-9); %-?
-%H = 10^9; %-?
-K_wear = 1.3*10^-17; %m^3 s^3 / kg^2
-TyreContactArea = 0.06; %m^2
-TyreRadius = 0.17; %m
-TyreArea = TyreRadius * TyreRadius * pi; %m^2 
-TyreWidth = 0.2; %m
+K_wear = 1.3*10^-17; % [m^3 s^3 / kg^2] --------coefficient that determines the wear
+TyreContactArea = 0.06; % [m^2] --------area of contact between tyre and asphalt
+%% Track parameters
+[roadS1, roadS2, roadS3, roadS4, curveRays, curveCenters, curveZ, zOffset, ...
+    xMax, xMin, yMax, yMin, bankingAngle] = track3d();
+% create the track initializing all the coordinates and parameters for the
+% shape of the road
+
+%% Simulation parameters
+m_fuel = 60; % [kg]  --------mass of the fuel
+m_vehicle = 590; % [kg]  --------mass of only the vehicle
+m_passenger = 70; % [kg]  --------mass of the passenger
+mT = m_vehicle + m_fuel + m_passenger; % [kg]  --------total mass
+g = 9.81; % [m/s^2]  --------gravity acceleration
+a = 1.2; % [m]  --------distance betwen COG and front tyre
+b = 1.6; % [m]  --------distance betwen COG and rear tyre
+IT = 1400; % [kg m^2]  --------moment of inertia
+l_f = 0.45; % [-]   --------load distrib over front wheel
+l_r = 0.55; % [-]   --------load distrib over rear wheel
+
+Cx = 0.8; % [-] --------longitudinal drag coefficient
+Cz = 1.5; % [-]  --------vertical (lift) drag coefficient
+rho_0 = 1.225; % [kg/m^3]   --------air density non-slipstream
+rho_1 = 0.8; % [kg/m^3]     --------air density slipstream
+Area = 2; % [m^2]   --------surface of vehicle on which the air goes through
+
+distanceThreshold = 10; % [m]  --------distance below which slipstream is considered ON
+overlapThreshold = 1; % [m]  --------width below which slipstream is considered ON
+alpha = 0.7; % [-]  --------coefficient to change slipstream distance and widht during curves
+
+init_vel = 0.01; % [m/s]  --------initial velocity  
+init_x_pos = 0; % [m]  --------initial x position
+init_y_pos = -5.625; % [m]  --------initial y position
+init_yaw = 0; % [rad]  --------initial yaw angle
+
+bankingThresholdLong = 110; % [m]  --------distance on the long straight
+                            %              from the curve below which the banking starts
+bankingThresholdShort = 40; % [m]  --------distance on the short straight
+                            %              from the curve below which the banking starts
+
+C_fuel = 0.0000003; % [s^2/m^2] --------coefficient for fuel consumption
+
+slipstreamON = 1; % 1 if slipstream ON, 0 for OFF
+bankingON = 1; % 1 if slipstream ON, 0 for OFF
