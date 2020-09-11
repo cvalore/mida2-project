0a068cad815fb613ad1a192f487e38a7be0d4da6
Carmelo <cvalore7@gmail.com>
2020-09-11 23:20:10 +0200

    u

--- Model/Copy_of_LateralRegulatorProject.m
+++ Model/Copy_of_LateralRegulatorProject.m
@@ -34,66 +34,39 @@ k = 3;
 %% rescheduling
 Grho_0 = -Vx^2;
 C0 = simplify(-((Cf*Cr*Vx^2*(lf + lr))/(- Cf*m*Vx^2*lf + Cr*m*Vx^2*lr + Cf*Cr*lf^2 + 2*Cf*Cr*lf*lr + Cf*Cr*lr^2))^-1 * Grho_0);
-%% Vx 45
-Vx = 45;
-N45 = subs(Ns);
-D1_45 = subs(Ds1);
-D2_45 = subs(Ds2);
+%% Vx 75
+Vx = 75;
+N75 = subs(Ns);
+D1_75 = subs(Ds1);
+D2_75 = subs(Ds2);
 
-modelTF_45 = N45/(D1_45 * D2_45);
-modelTF_45_tf = tf([11929228425000 82654603200000 227448000000000], [881093700*3 115925764869*3/5 95963157000*3 0 0], 'Name', 'vel 45 m/s');
+modelTF_75 = N75/(D1_75 * D2_75);
+modelTF_75_tf = tf([54546159375000 221997672000000 631800000000000], [2447482500*3 38641921623*3 58897125000*3 0 0], 'Name', 'vel 75 m/s');
 
-%% Vx 50
-Vx = 50;
-N50 = subs(Ns);
-D1_50 = subs(Ds1);
-D2_50 = subs(Ds2);
+%% Vx 80
+Vx = 80;
+N80 = subs(Ns);
+D1_80 = subs(Ds1);
+D2_80 = subs(Ds2);
 
-modelTF_50 = N50/(D1_50 * D2_50);
-modelTF_50_tf = tf([16313325000000 101198448000000 280800000000000], [1087770000*3 25761281082*3 91072500000*3 0 0], 'Name', 'vel 50 m/s');
+modelTF_80 = N80/(D1_80 * D2_80);
+modelTF_80_tf = tf([66121267200000 251773516800000 718848000000000], [2784691200*3 206090248656*3/5 50917632000*3 0 0], 'Name', 'vel 80 m/s');
 
-%% Vx 55
-Vx = 55;
-N55 = subs(Ns);
-D1_55 = subs(Ds1);
-D2_55 = subs(Ds2);
+%% Vx 85
+Vx = 85;
+N85 = subs(Ns);
+D1_85 = subs(Ds1);
+D2_85 = subs(Ds2);
 
-modelTF_55 = N55/(D1_55 * D2_55);
-modelTF_55_tf = tf([21658041075000 121614292800000 339768000000000], [1316201700*3 141687045951*3/5 85667037000*3 0 0], 'Name', 'vel 55 m/s');
-
-%% Vx 60
-Vx = 60;
-N60 = subs(Ns);
-D1_60 = subs(Ds1);
-D2_60 = subs(Ds2);
-
-modelTF_60 = N60/(D1_60 * D2_60);
-modelTF_60_tf = tf([28058529600000 143902137600000 404352000000000], [1566388800*3 154567686492*3/5 79746768000*3 0 0], 'Name', 'vel 60 m/s');
-
-%% Vx 65
-Vx = 65;
-N65 = subs(Ns);
-D1_65 = subs(Ds1);
-D2_65 = subs(Ds2);
-
-modelTF_65 = N65/(D1_65 * D2_65);
-modelTF_65_tf = tf([35609943525000 168061982400000 474552000000000], [1838331300*3 167448327033*3/5 73311693000*3 0 0], 'Name', 'vel 65 m/s');
-
-%% Vx 70
-Vx = 70;
-N70 = subs(Ns);
-D1_70 = subs(Ds1);
-D2_70 = subs(Ds2);
-
-modelTF_70 = N70/(D1_70 * D2_70);
-modelTF_70_tf = tf([44407435800000 194093827200000 550368000000000], [2132029200*3 180328967574*3/5 66361812000*3 0 0], 'Name', 'vel 70 m/s');
+modelTF_85 = N85/(D1_85 * D2_85);
+modelTF_85_tf = tf([79227912225000 283421361600000 811512000000000], [3143655300*3 218970889197*3/5 42423333000*3 0 0], 'Name', 'vel 85 m/s');
 
 %%
 figure(1)
 hold on
 legend
-vel = [45 50 55 60 65 70 75];
-models = [modelTF_45_tf modelTF_50_tf modelTF_55_tf modelTF_60_tf modelTF_65_tf modelTF_70_tf];
+vel = [75 80 85];
+models = [modelTF_75_tf modelTF_80_tf modelTF_85_tf];
 for i = 1:size(models, 2)
    bode(models(1,i));
 end
@@ -109,6 +82,8 @@ end
 
 % usato in model 3 con doppio integratore, zeri in 0.01 Hz gain 1.2*10^-4
 Reg = tf([0.030394 0.00381931 0.000119984], [1 0 0], 'Name', 'Regulator');
+
+% Reg = tf([0.016465 0.00206899 0.0000649974], [1 0 0], 'Name', 'Regulator');
 %%
 figure(2)
 hold on
